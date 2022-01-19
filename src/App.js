3b427c02f1df0d3c131fb7a3bad5acc9d27c69af
wizard chiboy <doncodesacademy@gmail.com>
2022-01-19 12:57:06 +0100

    some commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import SideBar from "./components/SideBar";
 import Chat from "./components/Chat";
 
 function App() {
+
+  useEffect(()=> {
+
+    const pusher = new Pusher('7ae261aa9a96b20010db', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', () => {
+      alert(JSON.stringify(data));
+    });
+
+  }, [])
   return (
     <>
       <div className="app">
